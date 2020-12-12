7a87a142bd38abfc5778386ab7b4e47951e3c0f2
anamfazal <anamfazal94@gmail.com>
2020-12-12 19:55:55 +0530

    [myramoon]Removed: few unnecessary files

--- mysite/api/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import Account
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = Account
-        fields = UserCreationForm.Meta.fields + ("email",)
--- mysite/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Account
-
-class AccountDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Account
-        fields = ('id','username', 'email' , 'password','date_joined','last_login')
